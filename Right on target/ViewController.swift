25a8cdf0b26665a6f5f5d5c76fabc54c1df56e5c
gribanskij <santygo@tut.by>
2024-02-28 16:23:19 +0300

    test

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -13,22 +13,62 @@ class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
     
     private var number:Int = 0
-    private var round:Int = 0
+    private var round:Int = 1
     private var points:Int = 0
     
+
+    
+    
+
+    override func loadView() {
+        super.loadView()
+        
+        //let versionlabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        //versionlabel.text = "Версия 1.1"
+        //view.addSubview(versionlabel)
+        
+        print("Load View")
+    }
     
     
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDdidLoad")
+        
+        number = Int.random(in: 1...50)
+        label.text = String(number)
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
     
     
     
     @IBAction func checkNumber(){
         
-        if round == 0 {
-            round = Int.random(in: 1...50)
-            label.text = String(number)
-            round = 1
-        } else {
             let numSlider = Int(slider.value.rounded())
             
             if numSlider > number {
@@ -52,12 +92,14 @@ class ViewController: UIViewController {
                 round+=1
             }
             
-        }
         
         number = Int.random(in: 1...50)
         label.text = String(number)
         
         
     }
+    
+    
+    
 }
 
